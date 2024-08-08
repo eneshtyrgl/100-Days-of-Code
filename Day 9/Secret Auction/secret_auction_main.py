1bd83be1fae00118a514cb14c8eae844287c07c7
ns <eneshtyrgl@gmail.com>
2024-08-08 19:02:53 +0300

    complete day 9

---
+++ Day 9/Secret Auction/secret_auction_main.py
@@ -0,0 +1,33 @@
+import os
+from time import sleep
+from art import logo
+
+os.system('cls')
+print("Welcome to the secret auction program.")
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+    print(logo)
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        sleep(1)
+        os.system('cls')
+input('Press ENTER to exit')
