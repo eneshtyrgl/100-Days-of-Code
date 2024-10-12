6bcdbdeca4eda8128faa7c8381b87f1a1a02db8c
eneshtyrgl <eneshtyrgl@gmail.com>
2024-10-12 14:39:33 +0300

    update location problem in main.py

--- Day 24/Mail Merge Project/main.py
+++ Day 24/Mail Merge Project/main.py
@@ -1,13 +1,19 @@
+import os
+
 PLACEHOLDER = "[name]"
 
+# Ensure the output directory exists
+output_dir = "Day 24/Mail Merge Project/Output/ReadyToSend"
+os.makedirs(output_dir, exist_ok=True)
 
-with open("100 Days of Code/Day 24/Mail Merge Project/Input/Names/invited_names.txt") as names_file:
+
+with open("Day 24/Mail Merge Project/Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
-with open("100 Days of Code/Day 24/Mail Merge Project/Input/Letters/starting_letter.txt") as letter_file:
+with open("Day 24/Mail Merge Project/Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        with open(f"100 Days of Code/Day 24/Mail Merge Project/Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+        with open(f"{output_dir}/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
