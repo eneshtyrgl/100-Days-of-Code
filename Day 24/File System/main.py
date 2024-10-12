3578339aacb278dd0a279c3775620c3a8e6ea7b4
eneshtyrgl <eneshtyrgl@gmail.com>
2024-10-12 14:42:38 +0300

    update location problem in main.py

--- Day 24/File System/main.py
+++ Day 24/File System/main.py
@@ -3,25 +3,25 @@
 
 # Open and Read File
 
-file = open("100 Days of Code\Day 24\File System\my_file.txt")
+file = open("Day 24/File System/my_file.txt")
 contents = file.read()
 print(contents)
 file.close()
 
-with open("100 Days of Code\Day 24\File System\my_file.txt") as file:
+with open("Day 24/File System/my_file.txt") as file:
     contents = file.read()
     print(contents)
 
 
 # Write on File   # modes:  r=read, w=write, a=append
 
-with open("100 Days of Code\Day 24\File System\my_file.txt", mode="w") as file:
+with open("Day 24/File System/my_file.txt", mode="w") as file:
     file.write("New text.")
 
-with open("100 Days of Code\Day 24\File System\my_file.txt", mode="a") as file:
+with open("Day 24/File System/my_file.txt", mode="a") as file:
     file.write("\nOther text.")
 
 
 # Create New File
-with open("100 Days of Code\Day 24\File System\other_file.txt", mode="w") as file:
+with open("Day 24/File System/other_file.txt", mode="w") as file:
     file.write("New file created.")
